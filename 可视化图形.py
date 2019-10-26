e3e464cda15a4558bb95e06031633e4ee6232dd1
rui-280 <1745841369@qq.com>
2019-10-26 16:32:34 +0800

    keshihua.py

---
+++ 可视化图形.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import matplotlib.pyplot as plt
+plt.plot([3,1,4,5,2])
+plt.ylabel("grade")
+plt.show()
+plt.savefig("text",dpi=600)
